701d3ae2621625a77d666878b54e2f4f102eb2e4
Arthur A. Gleckler <srfi@speechcode.com>
2020-06-08 20:27:37 -0700

    Copy sample implementation into repo.
    This is from <git@github.com:ar-nelson/scheme-threading-macros.git>.

---
+++ test.scm
@@ -0,0 +1,85 @@
+(include "./srfi-64-minimal.scm")
+(include "./threading-macros.scm")
+
+(define (exclamation x) (string-append x "!"))
+
+(test-begin "Threading Macros")
+
+(test-equal "->" "foobarbaz!"
+  (-> ""
+      (string-append "foo")
+      (string-append "bar")
+      (string-append "baz")
+      exclamation))
+
+(test-equal "->>" "bazbarfoo!"
+  (->> ""
+       (string-append "foo")
+       (string-append "bar")
+       (string-append "baz")
+       exclamation))
+
+(test-equal "as->" "barfoobaz"
+  (as-> "" x
+        (string-append x "foo")
+        (string-append "bar" x)
+        (string-append x "baz")))
+
+(test-equal "some->" "foobarbaz!"
+  (some-> ""
+          (string-append "foo")
+          (string-append "bar")
+          (string-append "baz")
+          exclamation))
+
+(test-equal "some-> short-circuit" #f
+  (some-> ""
+          (string-append "foo")
+          (equal? "bar")
+          (string-append "baz")
+          exclamation))
+
+
+(test-equal "some->>" "bazbarfoo!"
+  (some->> ""
+           (string-append "foo")
+           (string-append "bar")
+           (string-append "baz")
+           exclamation))
+
+(test-equal "some->> short-circuit" #f
+  (some->> ""
+           (string-append "foo")
+           (equal? "bar")
+           (string-append "baz")
+           exclamation))
+
+(test-equal "cond->" "foobaz!"
+  (cond-> ""
+          ((= (+ 2 2) 4) (string-append "foo"))
+          ((= (+ 2 2) 5) (string-append "bar"))
+          ((string-append "baz"))
+          (exclamation)))
+
+(test-equal "cond->>" "bazfoo!"
+  (cond->> ""
+           ((= (+ 2 2) 4) (string-append "foo"))
+           ((= (+ 2 2) 5) (string-append "bar"))
+           ((string-append "baz"))
+           (exclamation)))
+
+(test-equal "lambda->" "quxfoobarbaz!"
+  ((lambda-> (string-append "foo")
+             (string-append "bar")
+             (string-append "baz")
+             exclamation)
+   "qux"))
+
+(test-equal "lambda->>" "bazbarfooqux!"
+  ((lambda->> (string-append "foo")
+              (string-append "bar")
+              (string-append "baz")
+              exclamation)
+   "qux"))
+
+(test-end "Threading Macros")
